3fdbd97a0030ea771e0ca79771b71dacc3fc5329
chenuki <135982163+cypherfem@users.noreply.github.com>
2026-09-13 17:08:46 -0400

    Adding explore button.

--- src/main/java/com/chenuki/HomeView.java
+++ src/main/java/com/chenuki/HomeView.java
@@ -1,5 +1,7 @@
 package com.chenuki;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -11,12 +13,31 @@ public class HomeView extends VerticalLayout {
 
         Image homepage = new Image("/images/homepage.png", "Home");
         homepage.setWidth("100%");
-        homepage.setHeight("auto");
+
+        Div container = new Div();
+
+        container.setWidthFull();
+        container.getStyle().set("position", "relative");
+
+        Button exploreButton = new Button();
+
+        exploreButton.getStyle().set("position", "absolute");
+        exploreButton.getStyle().set("left", "22%");
+        exploreButton.getStyle().set("top", "9%");
+        exploreButton.getStyle().set("width", "12%");
+        exploreButton.getStyle().set("height", "6%");
+        exploreButton.getStyle().set("opacity", "15");
+
+        exploreButton.addClickListener(event -> {
+            getUI().ifPresent(ui -> ui.navigate("explore"));
+        });
+
+        container.add(homepage, exploreButton);
 
         setPadding(false);
         setSpacing(false);
         setWidthFull();
 
-        add(homepage);
+        add(container);
     }
 }
